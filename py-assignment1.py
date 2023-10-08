06bbc247ff0230aa78037192c7e81d5a3b8c4767
Shili88 <taili9736@gmail.com>
2023-10-08 09:15:12 +0100

    birthday assignment added

---
+++ py-assignment1.py
@@ -0,0 +1,14 @@
+""" 
+- Recipient's name
+- Year of birth
+- Personalized message
+- Sender's name
+"""
+
+# variable needed
+recipient_name = input("Hi, what's your name? ")
+year_of_birth = input("Please enter your year of birth. " )
+# personalized_messsage = ....
+# sender_name = ....
+
+# current_year = 2023 - int(year_of_birth)
